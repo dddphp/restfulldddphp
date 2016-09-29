732ee2345beffb2d3bb8cffc88c73a36cf87e57a
router <router@localhost.localdomain>
2016-09-28 22:43:22 -0300

    add framework folder

---
+++ framework/reflectiontools.php
@@ -0,0 +1,36 @@
+<?php
+function include_dir($path,$read=false) {
+	//separador de directorios
+	$s = '/';
+	//vemos si es la primera vez que usamos la funcion
+	if(!$read) {
+		//obtenemos los dos ultimos caracteres
+		$tree = substr($path,-2);
+		if($tree=='.*') {
+			//eliminamos el asterisco y activamos la recursividad
+			$path = preg_replace('!\.\*$!','',$path);
+			$read = true;
+		}
+		//obtenemos el document_root del archivo en caso de usarse
+		$path = preg_replace('!^root\.!',$_SERVER['DOCUMENT_ROOT'].$s,$path);
+		//cambiamos el punto por el separador
+		$path = str_replace('.',$s,$path);
+	}
+	//abrimos el directorio
+	if ($handle = opendir($path)) {
+		while (false !== ($file = readdir($handle))) {
+			if ($file != "." && $file != "..") {
+				//si es un directorio lo recorremos en caso de activar la recursividad
+				if(is_dir($path.$s.$file) and $read) {
+					include_dir($path.$s.$file,true);
+				} else {
+					$ext = substr(strtolower($file),-3);
+					if($ext == 'php') @include_once($path.$s.$file);
+				}
+			}
+		}
+		//cerramos el directorio
+		closedir($handle);
+	}
+}
+?>
